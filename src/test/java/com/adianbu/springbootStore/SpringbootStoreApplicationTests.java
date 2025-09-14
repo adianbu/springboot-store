f7c2339a7d0ce01730fc3b74c9125d670dfbac17
adianbu <theadithyaanbu@gmail.com>
2025-09-15 04:25:29 +0530

    Created Project

---
+++ src/test/java/com/adianbu/springbootStore/SpringbootStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adianbu.springbootStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
